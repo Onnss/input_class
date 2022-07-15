4afdb81f2cdba60e81de0695cc58bfbe010a45de
Onnss <onnsbenamara@gmail.com>
2022-07-15 20:56:00 +0100

    v1

---
+++ src/Component/modal.js
@@ -0,0 +1,25 @@
+function Modall(props) {
+    return (  <div><button  type="button" class="btn btn-primary w-50 mt-3 fs-4" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    submit
+  </button>
+
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Welcome</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body" >
+        {props.children}
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div> );
+}
+
+export default Modall;
