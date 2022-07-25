a91b48f4c39aef99b43397409a4fb33ac62adcb8
Onnss <onnsbenamara@gmail.com>
2022-07-25 15:43:10 +0100

    v1

--- src/Component/component.js
+++ src/Component/component.js
@@ -9,25 +9,27 @@ class Inputs extends React.Component{
     this.setState({
         name:event
     })
-}
-handleEmail=(event)=>{
-    this.setState({
-        email:event
-    })
-}
-handlePass=(event)=>{
-    this.setState({
-        password:event
-    })
-}
-
+    }
+    handleEmail=(event)=>{
+        this.setState({
+            email:event
+        })
+    }
+    handlePass=(event)=>{
+        this.setState({
+            password:event
+        })
+    }
     render (){
         return(
         <div className='container card shadow-lg p-3 mb-5 bg-body rounded mt-5  w-50'>
             <h1>Hello</h1>
-            <input onChange={(e)=>this.handleName(e.target.value)} type="text" className="mt-3 form-control"  placeholder="Full Name"></input>
-            <input onChange={(e)=>this.handleEmail(e.target.value)} type="email" className="mt-2 form-control"  placeholder="Email"></input>
-            <input onChange={(e)=>this.handlePass(e.target.value)} type="password" className="mt-2 form-control"  placeholder="Password"></input>
+            <input onChange={(e)=>this.handleName(e.target.value)} type="text" className="mt-3 form-control"  placeholder="Full Name" required/>
+            
+            <input onChange={(e)=>this.handleEmail(e.target.value)} type="email" className="mt-2 form-control"  placeholder="Email adress" required/>
+            
+            <input onChange={(e)=>this.handlePass(e.target.value)} type="password" className="mt-2 form-control"  placeholder="Password" required/>
+            
             <Modall> <h2> name : {this.state.name}</h2>
             <h2>email : {this.state.email}</h2>
             <h2>password: {this.state.password}</h2>
--- src/Component/modal.js
+++ src/Component/modal.js
@@ -1,20 +1,19 @@
 function Modall(props) {
-    return (  <div><button  type="button" class="btn btn-primary w-50 mt-3 fs-4" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    return (  <div><button  type="button" className="btn btn-primary w-50 mt-3 fs-4" data-bs-toggle="modal" data-bs-target="#exampleModal">
     submit
   </button>
 
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Welcome</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div className="modal-dialog">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalLabel">Welcome</h5>
         </div>
-        <div class="modal-body" >
+        <div className="modal-body" >
         {props.children}
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         </div>
       </div>
     </div>
